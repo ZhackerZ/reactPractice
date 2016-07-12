e281f197306f4834637acc9f3dffee79f6e8b82f
Yuzhen <liu.2699@buckeyemail.osu.edu>
2016-07-11 19:56:27 -0700

    ISOMORPHIC

--- program.js
+++ program.js
@@ -1,4 +1,22 @@
-var express = require('express');
+    var React = require('react');
+    var ReactDOMServer = require('react-dom/server');
+    var DOM = React.DOM;
+    var body = DOM.body;
+    var div = DOM.div;
+    var script = DOM.script;
+    
+    var browserify = require('browserify');
+    var babelify = require("babelify");
+
+    require('babel/register')({
+        ignore: false
+    });
+    
+    var TodoBox = require('./views/index.jsx');
+
+
+
+    var express = require('express');
     var app = express();
     
     app.set('port', (process.argv[2] || 3000));
@@ -15,6 +33,39 @@ var express = require('express');
 	});
     
     app.listen(app.get('port'), function() {});
+
+    app.use('/bundle.js', function (req, res) {
+        res.setHeader('content-type', 'application/javascript');
+    
+        browserify({ debug: true })
+            .transform(babelify.configure({
+                presets: ["react", "es2015"],
+                compact: false
+            }))
+            .require("./app.js", { entry: true })
+            .bundle()
+            .pipe(res);
+    });
+    
+    app.use('/', function (req, res) {
+        var initialData = JSON.stringify(data);
+        var markup = ReactDOMServer.renderToString(React.createElement(TodoBox, {data: data}));
+    
+        res.setHeader('Content-Type', 'text/html');
+    
+        var html = ReactDOMServer.renderToStaticMarkup(body(null,
+            div({id: 'app', dangerouslySetInnerHTML: {__html: markup}}),
+            script({
+                id: 'initial-data',
+                type: 'text/plain',
+                'data-json': initialData
+            }),
+            script({src: '/bundle.js'})
+        ));
+    
+        res.end(html);
+    });
+
 
 
 
